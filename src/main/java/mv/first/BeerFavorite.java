0d69f828a4c418f0583f18ea8c4ba95375a58684
ZorianaDatsko <127958586+ZorianaDatsko@users.noreply.github.com>
2023-11-30 13:07:26 +0200

    second commit;

--- src/main/java/mv/first/BeerFavorite.java
+++ src/main/java/mv/first/BeerFavorite.java
@@ -2,7 +2,7 @@ package mv.first;
 
 import com.github.javafaker.Faker;
 
-public class testClass_1 {
+public class BeerFavorite {
 
     public static void main(String[] args) {
         System.out.println(Faker.instance().beer().name());
